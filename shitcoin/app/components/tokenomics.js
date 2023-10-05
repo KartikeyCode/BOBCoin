7cd23e16a8595ce7f5906860fece74d489c377d1
unknown <Kartikey>
2023-10-05 21:41:25 +0530

    add tokenomics

--- shitcoin/app/components/tokenomics.js
+++ shitcoin/app/components/tokenomics.js
@@ -7,23 +7,9 @@ export default function Tokenomics() {
       <h1 className="mt-4 xl:mt-8 font-bold text-white font-Orbitron md:text-5xl xl:text-5xl">
         Total Supply: 1,000,000,000
       </h1>
+        <h1 className="mt-5  font-bold text-white font-Orbitron md:text-5xl xl:text-7xl"> {"TAX 9/9 =>"} </h1>
+        <div id="dotted" className="outline-dotted outline-4 outline-white p-1 mt-8 rounded-lg"> 
       <div className="xl:my-8 xl:flex justify-around text-center ">
-        <div
-          id="Tokenomics"
-          className="mr-2 my-4 rounded-2xl bg-gradient-to-r from-[#fb7405] via-[#DBA514] to-[#ffa617] p-1 shadow-xl"
-        >
-          <div className="block rounded-xl xl:w-72 bg-[#F8F1F1] p-4 sm:p-6 lg:p-8">
-            <div className="py-4">
-              <h3 className="md:text-2xl font-bold text-black xl:text-3xl sm:text-xl">
-                TAX
-              </h3>
-
-              <p className="md:text-lg mt-2 text-sm xl:text-2xl text-gray-500">
-                9/9
-              </p>
-            </div>
-          </div>
-        </div>
         <div className="mr-2 my-4 rounded-2xl bg-gradient-to-r from-[#fb7405] via-[#DBA514] to-[#ffa617] p-1 shadow-xl">
           <div className="block rounded-xl xl:w-72 bg-[#F8F1F1] p-4 sm:p-6 lg:p-8">
             <div className="py-4">
@@ -81,5 +67,6 @@ export default function Tokenomics() {
         </div>
       </div>
     </div>
+        </div>
   );
 }
